5e8a47d74b1475de6aa12208468676023cd96395
ZAKAR <saojung518@naver.com>
2024-02-19 02:22:16 +0900

    routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+import Navigation from "../components/navigation"
 
 export default function Tomato() {
-    return <h1>Hello NextJS!</h1>
+    return (
+        <div>
+            <Navigation />
+            <h1>Hello!</h1>
+        </div>
+    )
 }
