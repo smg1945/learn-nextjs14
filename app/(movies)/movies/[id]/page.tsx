f1436f29c2d9f92de10516687d2dc866e2d8765e
zakar <saojung518@naver.com>
2024-02-22 18:14:45 +0900

    css_modules

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,21 @@
-export default function MovieDetail({
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/movie-info";
+import MovieVideos from "../../../../components/movie-videos";
+
+export default async function MovieDetail({
   params: {id}
 }: {
   params: {id:string}
 }) {
-  return <h1>Movie {id}</h1>
+  return (
+    <div>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id}/>
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id}/>
+      </Suspense>
+    </div>
+  )
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
+
 export default function Navigation() {
     const path = usePathname()
     return (
